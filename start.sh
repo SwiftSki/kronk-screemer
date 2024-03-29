c7640e9fdda52bc54244dcedcb2133d6fba754b8
Ian Swift <ian.swift@student.nmt.edu>
2024-03-29 01:41:47 -0600

    update

--- start.sh
+++ start.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
-#REMEMBER TO COMMENT OUT ALL VENV STUFF WHEN PUSHING CODE
-
 #start server
-# source ./venv/bin/activate
-python /home/pi/Documents/kronk-screemer/bot.py
+DIRNAME=$(dirname $(realpath $0))
+# echo $DIRNAME
+
+# python /home/pi/Documents/kronk-screemer/bot.py
+python ${DIRNAME}/bot.py
 myvar=$?
 # echo exit code: $myvar
-if [ $myvar == '2' ] || [ $myvar == '1' ]; then
-    echo "bot.py probably not being run on server. Running from current directory"
-    python bot.py
-    myvar=$?
-fi
+# if [ $myvar == '2' ] || [ $myvar == '1' ]; then
+#     echo "bot.py probably not being run on server. Running from current directory"
+#     python bot.py
+#     myvar=$?
+# fi
 
 if [ $myvar = '12' ]; then
     echo "updating..."
-    cp token.txt ../
+    cp token.txt ${DIRNAME}/..
     git pull
-    cp ../token.txt ./
-#    deactivate
+    cp ${DIRNAME}/../token.txt ${DIRNAME}/
     ScriptLoc=$(readlink -f "$0")
     exec "$ScriptLoc"
 elif [ $myvar = '13' ]; then
     echo "shutting down"
-#    deactivate
     shutdown
 else
     echo "server stopped"
     echo "exit code: $myvar"
-#    deactivate
 fi
